2267326a084f543b65d7fe22e3b3fd2baa652e4e
poornimaj-star <PoornimaJ@first-insight.com>
2025-11-11 11:04:31 +0530

    base url changed

--- src/components/config.ts
+++ src/components/config.ts
@@ -1,4 +1,4 @@
 export class Constants {
-static  API_BASE_URL = 'https://nlqsqlserver.onrender.com';
+static  API_BASE_URL = 'https://nlqsqlserver-2.onrender.com';
 // static  API_BASE_URL = 'http://localhost:3000';
 }
